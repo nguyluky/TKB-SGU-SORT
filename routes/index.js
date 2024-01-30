fabe88c6ac9a3ca1cb083bdde60412650f32d71d
nguyluky <nguyluly@gmail.com>
2024-01-30 13:07:20 +0700

    remove templay

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const {whoami} = req.query;
   console.log(whoami)
-  res.render("index", {title: "hello", whoami: whoami});
-  // res.status(200).send()
+  res.render("index", {});
 });
 
 module.exports = router;

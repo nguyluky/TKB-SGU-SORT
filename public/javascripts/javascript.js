f05fce425b99c1a26412029c30d14978d615f743
nguyluky <nguyluly@gmail.com>
2024-01-30 18:54:01 +0700

    siderbar update

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -9,6 +9,7 @@ function hind_popup() {
         e.style.display = "none"
     })
     popup_is_show = false
+    popup_show = null
     document.querySelector('body').addEventListener('click', hind_popup)
 }
 
@@ -17,21 +18,44 @@ document.querySelectorAll('.sidebar-item').forEach(ele => {
     if (!menu) return
     menu.addEventListener('mouseenter', () => {
         if (!popup_is_show) return
+
+
         var e = ele.querySelector('.popup')
-        if (!e) return // nếu nó enter vào cái không có popup thì không làm gì hết
+
+        // nếu nó enter vào cái không có popup thì không làm gì hết
+        if (!e) return 
+        
         if (popup_show) {
             popup_show.style.display = 'none'
         }
+
         popup_show = e
         e.style.display = 'block'
     })
 
-    menu.addEventListener('click', (event) => {
-        popup_is_show = true
-        popup_show = ele.querySelector('.popup')
+    menu.addEventListener('click', (event) => {        
+        e = ele.querySelector('.popup')
+        
+        // nếu nó không có popup thì không làm gì cả
+        if (!e) return
+
+        // nếu nhân vào cái đã hiện thị rồi hì ẩn
+        if (popup_show == e) {
+            hind_popup()
+            return;
+        } 
+        popup_show = e
         popup_show.style.display = 'block'
+        popup_is_show = true
+
         event.stopPropagation()
+
         document.querySelector('body').addEventListener('click', hind_popup)
 
     })
 })
+
+
+document.getElementById('accout').onclick = () => {
+    console.log('login')
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(whoami)
+  // console.log(whoami)
   res.render("index", {});
 });
 

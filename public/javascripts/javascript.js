ca2dea18f1534047de3c3d88a8450c4774e69d0a
nguyluky <nguyluly@gmail.com>
2024-01-30 12:26:15 +0700

    html change

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,7 +1,36 @@
-var body = document.querySelector("body");
-var sidebar = body.querySelector(".sidebar");
-var iconarrow = body.querySelector(".iconarrow");
 
-iconarrow.addEventListener("click", () =>{
-        sidebar.classList.toggle('close');
-});
+// popup handel
+
+var popup_is_show = false
+var popup_show = null
+
+function hind_popup() {
+    document.querySelectorAll('.popup').forEach((e) => {
+        e.style.display = "none"
+    })
+    popup_is_show = false
+    document.querySelector('body').addEventListener('click', hind_popup)
+}
+
+document.querySelectorAll('.sidebar-item').forEach(ele => {
+    var menu = ele.querySelector('.sidebar-menu')
+    if (!menu) return
+    menu.addEventListener('mouseenter', () => {
+        if (!popup_is_show) return
+        if (popup_show) {
+            popup_show.style.display = 'none'
+        }
+        var e = ele.querySelector('.popup')
+        popup_show = e
+        e.style.display = 'block'
+    })
+
+    menu.addEventListener('click', (event) => {
+        popup_is_show = true
+        popup_show = ele.querySelector('.popup')
+        popup_show.style.display = 'block'
+        event.stopPropagation()
+        document.querySelector('body').addEventListener('click', hind_popup)
+
+    })
+})

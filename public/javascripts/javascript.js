842beaedcb4dd4aaa11313a6f016429e66f7c6bb
nguyluky <nguyluly@gmail.com>
2024-02-06 14:25:38 +0700

    add funs them hoc phan

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,8 +1,6 @@
 
 // popup handel
 
-var popup_is_show = false
-var popup_show = null
 var data = null
 
 /*Popup - Thêm học phần*/
@@ -35,85 +33,102 @@ var data = null
 /*Popup - Thêm học phần*/
 
 function checkLogin() {
-    return false
+    return true
 }
 
-function accout_hind() {
-    document.querySelector('.siderbar-right-item .accout-popup').style.display = 'none'
-    document.querySelector('body').addEventListener('click', accout_hind)
+function initAccoutClick() {
 
-}
+    var accountPopup = document.querySelector('.menubar-right-item .accout-popup')
 
-function hind_popup() {
-    document.querySelectorAll('.popup').forEach((e) => {
-        e.style.display = "none"
-    })
-    popup_is_show = false
-    popup_show = null
-    document.querySelector('body').addEventListener('click', hind_popup)
-}
-
-document.querySelectorAll('.menubar-item').forEach(ele => {
-    var menu = ele.querySelector('.menubar-menu')
-    if (!menu) return
-    menu.addEventListener('mouseenter', () => {
-        if (!popup_is_show) return
+    function accout_hind() {
+        accountPopup.style.display = 'none'
+        document.querySelector('body').addEventListener('click', accout_hind)
+    
+    }
 
 
-        var e = ele.querySelector('.popup')
+    document.getElementById('accout').onclick = (event) => {
 
-        // nếu nó enter vào cái không có popup thì không làm gì hết
-        if (!e) return
-
-        if (popup_show) {
-            popup_show.style.display = 'none'
+        // kiểm tra xem có đăng nhập chưa
+        if (!checkLogin()) {
+    
+            document.location.href = "/sign_up"
+            return
         }
-
-        popup_show = e
-        e.style.display = 'block'
-    })
-
-    menu.addEventListener('click', (event) => {
-        e = ele.querySelector('.popup')
-
-        // nếu nó không có popup thì không làm gì cả
-        if (!e) return
-
-        // nếu nhân vào cái đã hiện thị rồi hì ẩn
-        if (popup_show == e) {
-            hind_popup()
-            return;
+        if (accountPopup.style.display == 'block') {
+            accout_hind()
+            return
         }
-        popup_show = e
-        popup_show.style.display = 'block'
-        popup_is_show = true
-
+        accountPopup.style.display = 'block'
         event.stopPropagation()
+        document.querySelector('body').addEventListener('click', accout_hind)
+    }
+}
+initAccoutClick()
 
+
+function initMenuPopup() {
+    var popup_is_show = false
+    var popup_show = null   
+    
+    function hind_popup() {
+        document.querySelectorAll('.popup').forEach((e) => {
+            e.style.display = "none"
+        })
+        popup_is_show = false
+        popup_show = null
         document.querySelector('body').addEventListener('click', hind_popup)
+    }
 
+    
+    document.querySelectorAll('.menubar-item').forEach(ele => {
+        var menu = ele.querySelector('.menubar-menu')
+        if (!menu) return
+        menu.addEventListener('mouseenter', () => {
+            if (!popup_is_show) return
+
+
+            var e = ele.querySelector('.popup')
+
+            // nếu nó enter vào cái không có popup thì không làm gì hết
+            if (!e) return
+
+            if (popup_show) {
+                popup_show.style.display = 'none'
+            }
+
+            popup_show = e
+            e.style.display = 'block'
+        })
+
+        menu.addEventListener('click', (event) => {
+            e = ele.querySelector('.popup')
+
+            // nếu nó không có popup thì không làm gì cả
+            if (!e) return
+
+            // nếu nhân vào cái đã hiện thị rồi hì ẩn
+            if (popup_show == e) {
+                hind_popup()
+                return;
+            }
+            popup_show = e
+            popup_show.style.display = 'block'
+            popup_is_show = true
+
+            event.stopPropagation()
+
+            document.querySelector('body').addEventListener('click', hind_popup)
+
+        })
     })
-})
 
-
-document.getElementById('accout').onclick = (event) => {
-
-    // kiểm tra xem có đăng nhập chưa
-    if (!checkLogin()) {
-
-        document.location.href = "/sign_up"
-        return
-    }
-    if (document.querySelector('.siderbar-right-item .accout-popup').style.display == 'block') {
-        accout_hind()
-        return
-    }
-    document.querySelector('.siderbar-right-item .accout-popup').style.display = 'block'
-    event.stopPropagation()
-    document.querySelector('body').addEventListener('click', accout_hind)
 }
 
-document.getElementById('mod').onclick = (event) => {
+initMenuPopup()
+
+
+document.getElementById('theme').onclick = (event) => {
     if (localStorage.getItem('theme') != 'dark') {
         setToDarkMode();
         localStorage.setItem('theme', 'dark')
@@ -125,7 +140,6 @@ document.getElementById('mod').onclick = (event) => {
 }
 
 const sidebar = document.getElementById('siderbar')
-const resize = document.getElementById('resize')
 
 document.querySelector('.menubar-icon').onclick = () => {
     if (sidebar.classList.contains('close')) {
@@ -207,4 +221,55 @@ const tkb = {
     }
 
 
+}
+
+
+function hocphanpopup(e) {
+    console.log(e);
+    var parent = e.parentElement
+    parent.querySelector('.list-hp').classList.toggle('close')
+    e.querySelector('i').classList.toggle('close')
+}
+
+function addHp(mahp) {
+    var list = data.ds_nhom_to.filter(e => e.ma_mon == mahp)
+    var name = data.ds_mon_hoc[mahp]
+    var ct = list[0].so_tc
+    var div_hp = document.createElement('div')
+    div_hp.className = "hp"
+    var div_info = document.createElement('div')
+    div_info.className = "info"
+    div_info.innerHTML = `
+        <span class="name">${name}</span>
+        <span class="ct">${ct}ct</span>
+        <i class='bx bx-chevron-down close'></i>
+    `
+
+    div_info.onclick = () => {hocphanpopup(div_info)}
+
+    var div_list_hp = document.createElement('div')
+    div_list_hp.className = "list-hp close"
+
+    list.forEach(e => {
+        var div_hp_item = document.createElement('div')
+        div_hp_item.className = "list-hp-item"
+
+
+        var dsThu = [...new Set(e.tkb.map(e => e.thu))]
+        var dsGv = [...new Set(e.tkb.map(e => `<p> - ${e.gv} ${e.th ? '(TH)' : ''}</p>`))]
+
+        div_hp_item.innerHTML = `
+            <p>Thứ:  ${dsThu.join(' và ')}</p>
+            <p>GV:  </p>
+            ${dsGv.join('\n')}
+            <p>20/40</p>
+        `
+
+        div_list_hp.appendChild(div_hp_item)
+    })
+
+    div_hp.appendChild(div_info)
+    div_hp.appendChild(div_list_hp)
+
+    document.querySelector('.siderbar-body .ls').appendChild(div_hp)
 }

878ad229f6d83740bee1cb2b553017f170616e81
nguyluky <nguyluky@gmail.com>
2024-02-20 18:37:23 +0700

    update

--- db/redis.js
+++ db/redis.js
@@ -3,6 +3,8 @@ const RedisStore = require("connect-redis").default
 const {createClient} = require('redis')
 
 
+console.log(`redis://${process.env.REDIS_USER_NAME}:${process.env.REDIS_PASS}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`)
+
 let redisClient = createClient({
     url: `redis://${process.env.REDIS_USER_NAME}:${process.env.REDIS_PASS}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 })
